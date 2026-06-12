41ae36f73d522549dd923ed7c597150768e51491
erngrzab <erngrzab@amazon.com>
2026-06-11 19:46:50 -0600

    Initial Commit

---
+++ learningapp/learningappApp.swift
@@ -0,0 +1,32 @@
+//
+//  learningappApp.swift
+//  learningapp
+//
+//  Created by Garza, Ernesto on 11/06/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct learningappApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
